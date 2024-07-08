a02ba6477249f6249368a636f2e8905520d83dc4
PedroVieiraS <pedrocorrea978@gmail.com>
2024-07-08 20:12:09 -0300

    teste

---
+++ poo/src/main/java/com/poo/hotel/poo/PooApplication.java
@@ -0,0 +1,34 @@
+package com.poo.hotel.poo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PooApplication {
+	
+	public class LimpaTerminal {
+		static void limpa_windows() {
+			try {
+				if (System.getProperty("os.name").contains("Windows")) {
+					new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+				} else {
+					System.out.println("Este comando só funciona no Windows.");
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(PooApplication.class, args);
+
+
+		LimpaTerminal.limpa_windows();
+
+		System.out.println("ola");
+	}
+
+}
